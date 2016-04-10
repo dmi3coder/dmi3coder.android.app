89dab35af9e4a0e322a992d09dd0ebb1d9ea7cb0
dmi3coder <dmi3coder@gmail.com>
2016-04-10 15:11:40 +0300

    Intial commit

---
+++ app/src/main/java/com/dmi3coder/dmi3coder/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dmi3coder.dmi3coder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
